c786c5d1d6a1bf4027dd4e9ec04daa617a6dcc40
Dan Gibbons <dangibbo@amazon.co.uk>
2021-12-22 22:11:08 +0000

    Update Python code with updated version

--- functions/update_security_groups.py
+++ functions/update_security_groups.py
@@ -1,23 +1,36 @@
+'''
+Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+
+    http://aws.amazon.com/apache2.0/
+
+
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+'''
+
+# Ports your application uses that need inbound permissions from the service for
+# If all you're doing is HTTPS, this can be simply { 'https': 443 }
+INGRESS_PORTS = { 'http' : 80, 'https': 443, 'example': 8080 }
+# Tags which identify the security groups you want to update
+# For a group to be updated it will need to have 3 properties that are true:
+# 1. It has to be tagged 'Protocol: X' (Where 'X' is one of your INGRESS_PORTS above)
+# 2. It has to be tagged 'Name: cloudfront_g' or 'Name: cloudfront_r'
+# 3. It has to be tagged 'AutoUpdate: true'
+# If any of these 3 are not true, the security group will be unmodified.
+GLOBAL_SG_TAGS = { 'Name': 'cloudfront_g', 'AutoUpdate': 'true' }
+REGION_SG_TAGS = { 'Name': 'cloudfront_r', 'AutoUpdate': 'true' }
+
 import boto3
 import hashlib
 import json
 import logging
 import urllib.request, urllib.error, urllib.parse
 import os
-import random
-import string
-
-INGRESS_PORTS = os.getenv( 'PORTS', "80").split(",")
-SERVICE = os.getenv( 'SERVICE', "CLOUDFRONT")
-NAME = os.getenv( 'PREFIX_NAME', "AUTOUPDATE")
-VPC_ID= os.getenv( 'VPC_ID',"")
-REGION= os.getenv( 'REGION',"us-east-1")
-NRANGES=0
-NRULES=60
 
 def lambda_handler(event, context):
-    
-    global NRANGES
     # Set up logging
     if len(logging.getLogger().handlers) > 0:
         logging.getLogger().setLevel(logging.ERROR)
@@ -30,219 +43,32 @@ def lambda_handler(event, context):
             logging.getLogger().setLevel(logging.INFO)
     except KeyError:
         pass
-    # SNS message notification event when the ip ranges document is rotated
+
+    # If you want a different service, set the SERVICE environment variable.
+    # It defaults to CLOUDFRONT. Using 'jq' and 'curl' get the list of possible
+    # services like this:
+    # curl -s 'https://ip-ranges.amazonaws.com/ip-ranges.json' | jq -r '.prefixes[] | .service' ip-ranges.json | sort -u 
+    SERVICE = os.getenv( 'SERVICE', "CLOUDFRONT")
+    
     message = json.loads(event['Records'][0]['Sns']['Message'])
-    
+
+    # Load the ip ranges from the url
     ip_ranges = json.loads(get_ip_groups_json(message['url'], message['md5']))
-    cf_ranges = get_ranges_for_service(ip_ranges, SERVICE)
-    
-    #Number of security group rules required as per the total range count
-    NRANGES=len(cf_ranges)*len(INGRESS_PORTS)
-    
-    #Update SGs with the new ranges
-    update_security_groups(cf_ranges)
 
-def update_security_groups(new_ranges):
-    global VPC_ID
-    
-    #Creating ec2 boto3 client 
-    client = boto3.client('ec2',region_name=REGION)
-    
-    if VPC_ID=="":
-        result=client.describe_vpcs(Filters=[{'Name':'isDefault','Values': ['true']},])
-        VPC_ID=result["Vpcs"][0]['VpcId']
+    # Extract the service ranges
+    global_cf_ranges = get_ranges_for_service(ip_ranges, SERVICE, "GLOBAL")
+    region_cf_ranges = get_ranges_for_service(ip_ranges, SERVICE, "REGION")
 
-    #To number of SGs to update
-    rangeToUpdate = get_security_groups_for_update(client,True)
+    # Update the security groups
+    result = update_security_groups(global_cf_ranges, "GLOBAL")
+    result = result + update_security_groups(region_cf_ranges, "REGION")
+    
+    return result
 
-    if len(rangeToUpdate) == 0:
-            
-            logging.warning( 'No groups to {}'.format("update") )
-    else:
-        update_security_group(client, rangeToUpdate, new_ranges)
-        
-        
-def update_security_group(client, rangeToUpdate, new_ranges):
-
-        old_prefixes = list()
-        to_revoke = {}
-        to_add = list()
-        final_add={}
-        total=0
-        
-        for each_grp in rangeToUpdate['SecurityGroups']:
-            to_revoke[each_grp['GroupId']]=set()
-            
-            # If there are any exixting ranges in the SG, compares and add it to the revoke list if necessary
-            to_revoke_sg=0
-            if len(each_grp['IpPermissions']) > 0:
-                
-                for permission in each_grp['IpPermissions']:
-                    
-                    for range in permission['IpRanges']:
-                            cidr = range['CidrIp']
-                            old_prefixes.append(cidr)
-                            if new_ranges.count(cidr) == 0:
-                                to_revoke_sg+=1
-                                to_revoke[each_grp['GroupId']].add(cidr)
-
-                #Available slots in the SGs are the rules are revoked
-                remain_rules=NRULES-(len(each_grp['IpPermissions'][0]['IpRanges'])*len(INGRESS_PORTS))+to_revoke_sg
-                logging.info(("Total number of rules available in " + each_grp['GroupId'] + "are " + str(remain_rules)))
-                final_add[each_grp['GroupId']]=remain_rules
-                total+=remain_rules
-                
-            else:
-                final_add[each_grp['GroupId']]=NRULES
-                total+=NRULES
-        #Compares and identifies the new range to add from the service ranges list    
-        for range in new_ranges:
-            if (old_prefixes.count(range) == 0):
-                to_add.append({ 'CidrIp': range })
-                logging.info((" Range to be added: " + range))
-        count=0
-        for group in to_revoke:
-            if len(to_revoke[group])>0:
-                count+=len(to_revoke[group])
-                logging.info(("Rules that have to be revoked for  " + str(to_revoke[group])))
-                revoke_permissions(client,group,to_revoke[group])
-            else:
-                logging.info(("No rules were identified to be revoked in the security group "+group))
-        
-        logging.info(("Total number of rules to be revoked in all the security groups are "  + str(count*len(INGRESS_PORTS))))
-        logging.info(("Total number of rules to be added "  + str(len(to_add)*len(INGRESS_PORTS))))
-        logging.info(("Rules to add "  + str(to_add)))
-
-        dynamic_rule_add(client, final_add,to_add,total)
-        
-def dynamic_rule_add(client,final_add,to_add,total):
-    
-    random_str=''.join(random.choices(string.ascii_uppercase +string.digits, k = 3))
-    
-    if total< (len(to_add)*len(INGRESS_PORTS)):
-        security_group = client.create_security_group(
-                                Description=NAME+"-"+random_str,
-                                GroupName=NAME+"-"+random_str,
-                                VpcId=VPC_ID,
-                                DryRun=False
-                                )
-        all_sgs=list(final_add.keys())
-        response = client.describe_network_interfaces(
-                                Filters=[
-                                        {
-                                            'Name': 'group-id',
-                                            'Values': all_sgs
-                                        },
-                                    ]
-                                 )
-        
-        final_add[security_group['GroupId']]=NRULES
-        all_sgs=list(final_add.keys())
-        
-        for each_eni in response['NetworkInterfaces']:
-            
-            response = client.modify_network_interface_attribute(
-                                    Groups=all_sgs,
-                                    NetworkInterfaceId=each_eni["NetworkInterfaceId"],
-                                )
-        
-    for each_grp in final_add:
-        
-        num_accomodate=final_add[each_grp]//len(INGRESS_PORTS)
-        remain_per_grp=final_add[each_grp]%len(INGRESS_PORTS)
-        logging.info(("Number of rules can security group "+each_grp +" accomodate: "+str(num_accomodate*len(INGRESS_PORTS))))
-        
-        
-        for each_proto in INGRESS_PORTS:
-            permission = { 'ToPort': int(each_proto), 'FromPort': int(each_proto), 'IpProtocol': 'tcp'}
-            add_params = {
-                'ToPort': permission['ToPort'],
-                'FromPort': permission['FromPort'],
-                'IpRanges': to_add[0:num_accomodate],
-                'IpProtocol': permission['IpProtocol']
-                }
-        
-            client.authorize_security_group_ingress(GroupId=each_grp, IpPermissions=[add_params])
-            logging.info(("Modified "  + str(len(to_add[0:num_accomodate]))+" rules on security group "+each_grp+" for the port "+each_proto))
-
-        to_add=to_add[num_accomodate:]    
-        
-
-def revoke_permissions(client,group,to_revoke):
-    
-        
-    #Revoked rules in each SG for every port number
-    for each_proto in INGRESS_PORTS:
-        permission = { 'ToPort': int(each_proto), 'FromPort': int(each_proto), 'IpProtocol': 'tcp'}
-        revoke_params = {
-            'ToPort': permission['ToPort'],
-            'FromPort': permission['FromPort'],
-            'IpRanges': [{'CidrIp': iprange} for iprange in to_revoke],
-            'IpProtocol': permission['IpProtocol']
-        }
-
-        client.revoke_security_group_ingress(GroupId=group, IpPermissions=[revoke_params])
-        
-        logging.info(("Revoked "  + str(len(to_revoke))+" rules from the security group "+group+" with port "+each_proto))
-        logging.info(("Ranges revoked from the security group "+group+" are: "+str(to_revoke)))
-
-def create_security_groups(client,response):
-    
-    num_sgs=len(response['SecurityGroups'])
-    logging.info(('Found ' + str(num_sgs) + ' security groups'))
-
-    total_sgs_required=NRANGES//NRULES
-    
-    if NRANGES%NRULES>0:
-        total_sgs_required+=1
-    logging.info(('Total number of security groups required to add all the rules: ' + str(total_sgs_required)))
-    
-    to_create_sgs=0
-    
-    if num_sgs<total_sgs_required:
-        to_create_sgs=total_sgs_required-num_sgs
-    logging.info(('Total number of security groups to be created: ' + str(to_create_sgs)))
-    
-    #Creates SGs based on the total number of rules that are required to be added
-    created_sgs=[]
-    
-    for sg in range(to_create_sgs):
-        random_str=''.join(random.choices(string.ascii_uppercase +string.digits, k = 3))
-        security_group = client.create_security_group(
-                                Description=NAME+"-"+random_str,
-                                GroupName=NAME+"-"+random_str,
-                                VpcId=VPC_ID,
-                                DryRun=False
-                                )
-        created_sgs.append(security_group['GroupId'])
-        response = client.create_tags(Resources=created_sgs,
-                                      Tags=[{
-                                                'Key': 'PREFIX_NAME',
-                                                'Value': NAME,
-                                            },
-                                    ],)
-    
-    return get_security_groups_for_update(client)
-        
-def get_security_groups_for_update(client,create=False):
-    filters = [
-                { 'Name': "tag-key", 'Values': ['PREFIX_NAME'] },
-                { 'Name': "tag-value", 'Values': [NAME] },
-                { 'Name': "vpc-id", 'Values': [VPC_ID] }
-            ]
-    
-    #Extracting specific security groups with tags 
-    response = client.describe_security_groups(Filters=filters)
-    
-    #Return list of all security groups if none to be craeted
-    if create==False:
-        return response
-    else:
-        return create_security_groups(client,response)
 
 def get_ip_groups_json(url, expected_hash):
     
-    logging.info("Updating from " + url)
+    logging.debug("Updating from " + url)
 
     response = urllib.request.urlopen(url)
     ip_json = response.read()
@@ -255,14 +81,151 @@ def get_ip_groups_json(url, expected_hash):
         raise Exception('MD5 Mismatch: got ' + hash + ' expected ' + expected_hash)
 
     return ip_json
-    
-def get_ranges_for_service(ranges, service):
+
+def get_ranges_for_service(ranges, service, subset):
     
     service_ranges = list()
-    
     for prefix in ranges['prefixes']:
-        if prefix['service'] == service:
+        if prefix['service'] == service and ((subset == prefix['region'] and subset == "GLOBAL") or (subset != 'GLOBAL' and prefix['region'] != 'GLOBAL')):
+            logging.info(('Found ' + service + ' region: ' + prefix['region'] + ' range: ' + prefix['ip_prefix']))
             service_ranges.append(prefix['ip_prefix'])
-    
-    logging.info(('Found ' + service + ' ranges: ' + str(len(service_ranges))))
+
     return service_ranges
+
+def update_security_groups(new_ranges, rangeType):
+    
+    client = boto3.client('ec2')
+    result = list()
+    
+    # All the security groups we will need to find.
+    allSGs = INGRESS_PORTS.keys()
+    # Iterate over every group, doing its global and regional versions
+    for curGroup in allSGs:
+        tagToFind = {}
+        if rangeType == "GLOBAL":
+            tagToFind = GLOBAL_SG_TAGS
+        else:
+            tagToFind = REGION_SG_TAGS    
+        tagToFind['Protocol'] = curGroup
+        rangeToUpdate = get_security_groups_for_update(client, tagToFind)
+        msg = 'tagged Name: {}, Protocol: {} to update'.format( tagToFind["Name"], curGroup )
+        logging.info('Found {} groups {}'.format( str(len(rangeToUpdate)), msg ) )
+
+        if len(rangeToUpdate) == 0:
+            result.append( 'No groups {}'.format(msg) )
+            logging.warning( 'No groups {}'.format(msg) )
+        else:
+            for securityGroupToUpdate in rangeToUpdate:
+                if update_security_group(client, securityGroupToUpdate, new_ranges, INGRESS_PORTS[curGroup] ):
+                    result.append('Security Group {} updated.'.format( securityGroupToUpdate['GroupId'] ) )
+                else:
+                    result.append('Security Group {} unchanged.'.format( securityGroupToUpdate['GroupId'] ) )
+
+    return result
+
+
+def update_security_group(client, group, new_ranges, port):
+    added = 0
+    removed = 0
+    
+    if len(group['IpPermissions']) > 0:
+        for permission in group['IpPermissions']:
+            if permission['FromPort'] <= port and permission['ToPort'] >= port:
+                old_prefixes = list()
+                to_revoke = list()
+                to_add = list()
+                for range in permission['IpRanges']:
+                    cidr = range['CidrIp']
+                    old_prefixes.append(cidr)
+                    if new_ranges.count(cidr) == 0:
+                        to_revoke.append(range)
+                        logging.debug((group['GroupId'] + ": Revoking " + cidr + ":" + str(permission['ToPort'])))
+
+                for range in new_ranges:
+                    if old_prefixes.count(range) == 0:
+                        to_add.append({ 'CidrIp': range })
+                        logging.debug((group['GroupId'] + ": Adding " + range + ":" + str(permission['ToPort'])))
+
+                removed += revoke_permissions(client, group, permission, to_revoke)
+                added += add_permissions(client, group, permission, to_add)
+    else:
+        to_add = list()
+        for range in new_ranges:
+            to_add.append({ 'CidrIp': range })
+            logging.info((group['GroupId'] + ": Adding " + range + ":" + str(port)))
+        permission = { 'ToPort': port, 'FromPort': port, 'IpProtocol': 'tcp'}
+        added += add_permissions(client, group, permission, to_add)
+
+    logging.debug((group['GroupId'] + ": Added " + str(added) + ", Revoked " + str(removed)))
+    return (added > 0 or removed > 0)
+
+
+def revoke_permissions(client, group, permission, to_revoke):
+    if len(to_revoke) > 0:
+        revoke_params = {
+            'ToPort': permission['ToPort'],
+            'FromPort': permission['FromPort'],
+            'IpRanges': to_revoke,
+            'IpProtocol': permission['IpProtocol']
+        }
+
+        client.revoke_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[revoke_params])
+
+    return len(to_revoke)
+
+
+def add_permissions(client, group, permission, to_add):
+    if len(to_add) > 0:
+        add_params = {
+            'ToPort': permission['ToPort'],
+            'FromPort': permission['FromPort'],
+            'IpRanges': to_add,
+            'IpProtocol': permission['IpProtocol']
+        }
+
+        client.authorize_security_group_ingress(GroupId=group['GroupId'], IpPermissions=[add_params])
+
+    return len(to_add)
+
+
+def get_security_groups_for_update(client, security_group_tag):
+    filters = list()
+    for key, value in security_group_tag.items():
+        filters.extend(
+            [
+                { 'Name': "tag-key", 'Values': [ key ] },
+                { 'Name': "tag-value", 'Values': [ value ] }
+            ]
+        )
+
+    response = client.describe_security_groups(Filters=filters)
+
+    return response['SecurityGroups']
+
+# This is a handy test event you can use when testing your lambda function.
+'''
+Sample Event From SNS:
+
+{
+  "Records": [
+    {
+      "EventVersion": "1.0",
+      "EventSubscriptionArn": "arn:aws:sns:EXAMPLE",
+      "EventSource": "aws:sns",
+      "Sns": {
+        "SignatureVersion": "1",
+        "Timestamp": "1970-01-01T00:00:00.000Z",
+        "Signature": "EXAMPLE",
+        "SigningCertUrl": "EXAMPLE",
+        "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+        "Message": "{\"create-time\": \"yyyy-mm-ddThh:mm:ss+00:00\", \"synctoken\": \"0123456789\", \"md5\": \"45be1ba64fe83acb7ef247bccbc45704\", \"url\": \"https://ip-ranges.amazonaws.com/ip-ranges.json\"}",
+        "Type": "Notification",
+        "UnsubscribeUrl": "EXAMPLE",
+        "TopicArn": "arn:aws:sns:EXAMPLE",
+        "Subject": "TestInvoke"
+      }
+    }
+  ]
+}
+
+'''
